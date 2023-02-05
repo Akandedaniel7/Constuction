9a1617a4b678153d14248716a3b6190494259c49
= <akandedaniel86@gmial.com>
2023-02-04 21:26:26 -0800

    commit

--- Constructio JS/Construction.js
+++ Constructio JS/Construction.js
@@ -15,3 +15,41 @@ window.addEventListener("scroll", () => {
     }
 })
 
+/* ================================ Contact Validation ================================= */
+const contactForm = document.getElementById("contact-form"),
+contactName = document.getElementById("contact-name"),
+contactEmail = document.getElementById("contact-email"),
+Message = document.getElementById("message"),
+contactMessage = document.getElementById("contact-message");
+
+e.preventDefault()
+
+ if (
+    contactName.value === '' ||
+    contactEmail.value === '' ||
+    Message.value === ''
+    ) {
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+
+    contactMessage.textContent = 'Write all the input fields'
+
+    else{
+      emailjs.sendForm('service_2zpsd8i',
+       'template_dx4ccww',
+        '#contact-form',
+         'PzQEiD2iWAjgdnbjt'
+         );
+         .then(() => {
+            contactMessage.classList.add('color-light');
+            contactMessage.textContent = 'Message Sent ✔';
+
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000);
+         })
+    }
+}
+
+
+contactForm.addEventListener('submit', sendEmail);
