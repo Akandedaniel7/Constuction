3d7c3f3ec44edd544792751dd7dedf2822dbc88c
devsammyy <yinkaadebayo31@gmail.com>
2023-02-06 12:28:02 +0100

    made tweaks

--- Constructio JS/Construction.js
+++ Constructio JS/Construction.js
@@ -1,58 +1,61 @@
 /* ================================= Section Background-image Changing =========================== */
-let images = [
-    'url('
-]
+let images = ["url("];
 
 /* ================================ Back-to-top ============================== */
 const backToTop = document.querySelector(".back-to-top");
 
 window.addEventListener("scroll", () => {
-    if ( window.pageYOffset > 100) {
-        backToTop.classList.add("active");
-    }
-    else{
-        backToTop.classList.remove("active");
-    }
-})
+  if (window.pageYOffset > 100) {
+    backToTop.classList.add("active");
+  } else {
+    backToTop.classList.remove("active");
+  }
+});
 
 /* ================================ Contact Validation ================================= */
-const contactForm = document.getElementById("contact-form"),
-contactName = document.getElementById("contact-name"),
-contactEmail = document.getElementById("contact-email"),
-Message = document.getElementById("message"),
-contactMessage = document.getElementById("contact-message");
+const contactForm = document.getElementById("contact-form");
+const contactName = document.getElementById("contactName");
+const contactEmail = document.getElementById("contactEmail");
+const message = document.getElementById("message"),
+  contactMessage = document.getElementById("contactMessage");
 
 const sendEmail = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (contactMe.value === "" || emailMe.value === ""|| messageMe.value === "" ) {
-         contactMessage.classList.remove('color-light');
-         contactMessage.classList.add('color-dark');
+  if (
+    contactName.value === "" &&
+    contactEmail.value === "" &&
+    contactMessage.value === ""
+  ) {
+    contactMessage.classList.remove("color-light");
+    contactMessage.classList.add("color-dark");
 
-        contactMessage.textContent = 'Write all the input fields';
-         }else{
-        emailjs
-         .sendForm('service_2zpsd8i', 
-            'template_dx4ccww',
-             '#contact-form', 
-             'PzQEiD2iWAjgdnbjt')
-        .then(
-            () =>{
-            contactMessage.classList.add('color-light');
-            contactMessage.textContent = 'Message sent';
+    contactMessage.textContent = "Write all the input fields";
+  } else {
+    emailjs
+      .sendForm(
+        "service_2zpsd8i",
+        "template_dx4ccww",
+        "contact-form",
+        "PzQEiD2iWAjgdnbjt"
+      )
+      .then(
+        () => {
+          contactMessage.classList.add("color-light");
+          contactMessage.textContent = "Message sent";
 
-            setTimeout(() =>{
-                contactMessage.textContent = '';
-            }, 5000);
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
         },
-         (error) => {
-            alert('OOPs! SOMETHING WENT WRONG...', error);
+        (error) => {
+          alert("OOPs! SOMETHING WENT WRONG...", error);
         }
-        );
-        nameMe.value = '';
-        emailMe.value = '';
-        messageMe.value = '';
-    }
+      );
+    contactName.value = "";
+    contactEmail.value = "";
+    contactMessage.value = "";
+  }
 };
 
-contactForm.addEventListener('submit', sendEmail);
+contactForm.addEventListener("submit", sendEmail);
