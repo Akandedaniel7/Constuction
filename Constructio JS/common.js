ab549a85524afc0be998d3fbdd1ae352739d4c70
Akande Daniel <akandedaniel86@gmial.com>
2023-03-03 11:45:17 +0100

    comit

--- Constructio JS/common.js
+++ Constructio JS/common.js
@@ -1,45 +1,4 @@
-/* =========== SHOW MENU  ========= */
-const navMenu = document.getElementById('nav-menu'),
-   navToggle = document.getElementById('nav__toggle'),
-   navClose = document.getElementById('nav__close');
-
-// Menue Show
-if (navToggle) {
-	navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu');
-	})
-}
-
-// Close Show
-if (navClose) {
-	navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu');
-	})
-}
-
-//Remove Menu Mobile
-const navLink = document.querySelectorAll('.nav__link');
-
-function linkAction(){
-	const navMenu = document.getElementById('nav-menu');
-	navMenu.classList.remove('show-menu');
-}
-
-navLink.forEach((n) => n.addEventListener('click', linkAction));
-
-
-/* =========== BACKGROUND HEADER  ========= */
-function scrollHeader(){
-  const header = document.getElementById('header');
-
-if (this.scrollY >= 50) header.classList.add('scroll-header');
-else header.classList.remove('scroll-header');
-}
-
-
-window.addEventListener("scroll", scrollHeader);
-
- /* =========== CONTACT FORM  ========= */
+/* =========== CONTACT FORM  ========= */
   const contactMe = document.getElementById('contact-form'),
  nameMe = document.getElementById('contact-name'),
   emailMe = document.getElementById('contact-email'),
