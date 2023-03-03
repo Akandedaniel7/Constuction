0b0bca65577320ee67fdda72113a8b5494068415
Akande Daniel <akandedaniel86@gmial.com>
2023-03-03 01:18:41 +0100

    commmit

--- Constructio JS/Construction.js
+++ Constructio JS/Construction.js
@@ -34,55 +34,23 @@ window.addEventListener("scroll", () => {
 });
 
 /* ============ SIDE MENU =========== */
-let sidemenu = document.getElementById("sidemenu");
+const sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
   sidemenu.style.right = '0';
 }
 
 function closemenu() {
-  sidemenu.style.right = '-200px';
+  sidemenu.style.right = '-100%';
 };
 
-/* =========== CONTACT FORM  ========= */
 
-const contactMe = document.getElementById('contact-form'),
- nameMe = document.getElementById('contact-name'),
-  emailMe = document.getElementById('contact-email'),
- messageMe = document.getElementById('message'),
- contactMessage = document.getElementById('contact-message');
 
- const sendEmail = (e) => {
- 	e.preventDefault();
+const navLink  = document.querySelectorAll('.nav-link');
 
- 	if (contactMe.value === "" || emailMe.value === ""|| messageMe.value === "" ) {
-  		contactMessage.classList.remove('color-light');
-  		contactMessage.classList.add('color-dark');
+function linkAction(){
+	const sidemenu = document.getElementById('sidemenu');
+     sidemenu.style.right = '-100%';
+}
 
- 		contactMessage.textContent = 'Write all the input fields';
-	  	}else{
- 		emailjs
-  		.sendForm('service_jagc8l2', 
- 			'template_xvw1iuv',
- 			 '#contact-form', 
- 			 'EOqB9s-ao-VmHaGia')
- 		.then(
- 			() =>{
- 			contactMessage.classList.add('color-light');
- 			contactMessage.textContent = 'Message sent';
-
- 			setTimeout(() =>{
- 				contactMessage.textContent = '';
- 			}, 5000);
- 		},
- 		 (error) => {
- 			alert('OOPs! SOMETHING WENT WRONG...', error);
- 		}
- 		);
- 		nameMe.value = '';
- 		emailMe.value = '';
- 		messageMe.value = '';
- 	}
- };
-
- contactMe.addEventListener('submit', sendEmail);
+navLink.forEach((n) => n.addEventListener('click', linkAction));
